de2241b8e5bf7922b9e73ca6d448c1071065c7f6
richiijs <richiijs@inbox.lv>
2023-05-05 06:23:36 +0300

    Update main.go

--- main.go
+++ main.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Get the generator early here! https://discord.gg/Uer33dUENA")
+    fmt.Println("Get the generator early here! https://discord.com/invite/Uer33dUENA")
 }
